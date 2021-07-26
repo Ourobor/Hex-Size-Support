675422e1e363f2babff470a7b91d2e42a9033a15
Michael Enion <caewok_2000@yahoo.com>
2021-07-26 14:41:57 -0700

    correct typos with calling libWrapper function

--- modules/patching.js
+++ modules/patching.js
@@ -2,9 +2,9 @@ import {HexSizeSupportAddWaypoint, HexSizeSupportMeasure, HexSizeSupportAnimateT
 
 const MODULE_ID = "hex-size-support";
 
-export function registerLibRuler() {
-  libwrapper.register(MODULE_ID, 'Ruler.prototype._addWaypoint', HexSizeSupportAddWaypoint, 'WRAPPER');
-  libwrapper.register(MODULE_ID, 'Ruler.prototype.measure', HexSizeSupportMeasure, 'WRAPPER');
+export function registerHexRulerPatches() {
+  libWrapper.register(MODULE_ID, 'Ruler.prototype._addWaypoint', HexSizeSupportAddWaypoint, 'WRAPPER');
+  libWrapper.register(MODULE_ID, 'Ruler.prototype.measure', HexSizeSupportMeasure, 'WRAPPER');
 
   // The animateToken method is added by libRuler module
   libWrapper.register(MODULE_ID, 'Ruler.prototype.animateToken', HexSizeSupportAnimateToken, 'WRAPPER');
